f2c1ed73e5e617803249fa434a85273c83525b5b
SHAIK RIYAN <shaikriyan@Cruzzs-MacBook-Air.local>
2026-06-22 15:35:15 +0530

    First commit

---
+++ output_input.py
@@ -0,0 +1,48 @@
+"""
+To display/output the values, we use print() function.
+
+"""
+
+
+
+print(1000)
+print(85.5)
+print("Hello everyone, good evening!")
+
+
+
+#variable
+name ="Vinay"
+print(name) 
+
+#This is my first script in python
+#This is called a comment 
+#We write single line comment using '#'
+
+
+#program to add 2 numbers
+a =10
+b =20
+c = a + b
+print(c)
+
+"""
+This is an example of multi-line comment in python.
+Anything written inside the triple quotes will be considered as a comment.
+"""
+print(10, 20, 30) # example of printing multiple values in the same line
+#in output we can see spaces between the numbers because byb default the separator is space. we can change the separetor using sep parameter in print function.
+
+age = 21
+print("My age is", age)
+
+
+"""
+input()
+Function to take input form the user.
+"""
+first_name = input("Enter your first name: ")
+last_name =input("Enter your last_name: ")
+# print(first_name)
+# print(last_name)
+print("Welcome", first_name, last_name)
